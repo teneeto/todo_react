f5732ecb96ef6f3d14a8e505ec3ebbcdb6fea7b0
Olei Eto <teneeto@gmail.com>
2019-07-21 01:08:34 +0100

    implemented routerfor home and about links

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Todos from './components/Todos';
 import './App.css';
 import Header from './components/layout/Header';
 import AddTodo from './components/AddTodo';
+import About from './components/pages/About'
 import uuid from 'uuid';
 
 class App extends Component {
@@ -58,13 +60,23 @@ class App extends Component {
   render() {
 
     return (
+      <Router>
       <div className="App" >
         <div className="container">
           <Header />
-          <AddTodo addTodo = {this.addTodo}/>
-          <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
+          <Route exact path="/" render={props => (
+            <React.Fragment>
+              <AddTodo addTodo = {this.addTodo}/>
+              <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
+            </React.Fragment>
+          )}/ >
+          
+          <Route path="/about" component={About} />
+            
+          
         </div>
       </div>
+      </Router>
     );
   }
 }
